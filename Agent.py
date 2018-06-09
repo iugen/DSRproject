96bb4c6acf0c05d00e97082a2fef40f2af9cb2d4
iugen <iugen@hotmail.it>
2018-06-09 10:15:36 +0200

    added target network

--- Agent.py
+++ Agent.py
@@ -2,26 +2,31 @@ import numpy as np
 from keras import models, layers, optimizers
 from collections import deque
 
-class agent_class(object):
+class AgentClass(object):
 	"""docstring for agent"""
-	def __init__(self):
+	def __init__(self,exploration):
 		self.actions_list = [0,1]   # stay/change
-		self.training_batch = deque(maxlen=500)
-		self.epsilon = 1
-		self.eps_decay = 0.95
+		self.training_memory = deque(maxlen=5000)
+		self.epsilon = exploration
+		self.eps_decay = 0.99
 		self.min_eps = 0.1
 		self.model = self.createNN()
+		self.target_model = self.createNN()
 		self.gamma = 0.9
 
 	def createNN(self):
 		model = models.Sequential()
-		model.add(layers.Dense(16,activation='relu',input_shape=(8,)))
-		#model.add(layers.Dense(32,activation='relu',))
+		model.add(layers.Dense(64,activation='relu',input_shape=(17,)))
+		model.add(layers.Dense(16,activation='relu',))
 		model.add(layers.Dense(2,activation='linear')) 
 		# I have only one traffic light. Simple strategy: stay/change
-		opt = optimizers.RMSprop(lr=0.001)
+		opt = optimizers.Adam(lr=0.001)
 		model.compile(optimizer = opt, loss='mse')
 		return model
+
+	def update_target_network(self):
+		#self.target_model = self.model
+		self.target_model.set_weights(self.model.get_weights()) 
 
 
 	def select_action(self,state):
@@ -34,37 +39,34 @@ class agent_class(object):
 		return action
 
 	
-	def memory(self,state,reward,next_s,a):
-		self.training_batch.append([state,reward,next_s,a])
+	def memory(self,state,reward,next_s,a,done):
+		self.training_memory.append([state,reward,next_s,a,done])
 
 
 	def fit_NN(self):
-		'''data = np.array(self.training_batch)
-		for state,reward,next_s,a in data:
-			Q1 = reward + self.gamma*np.max(self.model.predict(next_s))
-			Q = self.model.predict(state)
-			Q[0][a] = Q1            
-			self.model.fit(state,Q, epochs=1)
-		if self.epsilon > self.min_eps:
-			self.decay()'''
-		data = np.array(self.training_batch)    
-		Q_target = np.empty((data.shape[0], 2))
-		state_vector = np.empty((data.shape[0], 8))
+		data = np.array(self.training_memory)  
+		batch = data[np.random.randint(data.shape[0], size=32), :]
+
+		Q_target = np.empty((batch.shape[0], 2))
+		state_vector = np.empty((batch.shape[0], 17))
 		i=0
-		for state,reward,next_s,a in data:
-			Q1 = reward + self.gamma*np.max(self.model.predict(next_s))
-			Q = self.model.predict(state)
+		for state,reward,next_s,a,done in batch:
+			Q1 = reward + self.gamma*np.max(self.target_model.predict(next_s))*done
+			Q = self.target_model.predict(state)
 			Q[0][a] = Q1
 			state_vector[[i]] = state
 			Q_target[[i]] = Q
 			i += 1
-		self.model.fit(state_vector,Q_target, epochs=5)
-		if self.epsilon > self.min_eps:
-			self.decay()
-
+			self.model.fit(state_vector,Q_target, epochs=1, batch_size=batch.shape[0])
+			if self.epsilon > self.min_eps:
+				self.decay()
 
 	def decay(self):
 		self.epsilon = self.epsilon*self.eps_decay
 
+	def load(self, name):
+		self.model.load_weights(name)
 
+	def save(self, name):
+		self.model.save_weights(name)
 
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import sys
 import numpy as np
 import traci
-from Agent import agent_class
+from Agent import AgentClass
 
 		
 
@@ -14,15 +14,33 @@ def TL_state(detectors_ID):
 		# on the named induction loop within the last simulation step [m/s]
 		state.append(speed)
 
-	state = np.array(state)
+		n_veh = traci.lanearea.getLastStepVehicleNumber(detector)
+		# Returns the mean speed of vehicles that were 
+		# on the named induction loop within the last simulation step [m/s]
+		state.append(n_veh)
+
+	state.append(traci.trafficlight.getPhase(traci.trafficlight.getIDList()[0]))
+		
+
+	state = np.array(state,)
 	state = state.reshape((1, state.shape[0]))
 	return state
 
 
+def TL_reward(detectors_ID,previous_reward):
+	reward_sum = 0
+	for detector in detectors_ID:
+		reward = -traci.lanearea.getJamLengthVehicle(detector)
+		reward_sum += reward   
+	#reward_sum += previous_reward
+	return reward_sum
 
-def TL_reward(s):
-	reward = s.mean()
-	return reward
+	# reward_sum: ne passano solo due alla volta
+	# + phase: loss is nan
+	# += previous_reward: non cambia mai
+	# + phase: loss is nan<
+	# altre function?
+	# tolto primo if dopo while (faccio il training ogni secondo, invece che dieci)
 
 
 def run():
@@ -31,38 +49,49 @@ def run():
 	TL_id = traci.trafficlight.getIDList() # list of traffic lights (just one n this case)
 	total_step = 2000		# total step of the simulation
 	detectors_ID = traci.lanearea.getIDList()
-	n_epochs = 5
-	agent = agent_class()
+	n_episodes = 100
+	update_time = 10
+	agent = AgentClass(1)
 	traci.close()
 
-	for i in range(n_epochs):
+	for i in range(n_episodes):
 		traci.start(sumoCmd)
 		state = TL_state(detectors_ID)  
 		total_reward = 0
-		step = 0
-		number_of_phases = 6
-		int_step = 0
+		previous_reward = 0
+		step = 1
+		number_of_phases = 6    # TO DO: get it from traci
+		done = 1
 
-		while step < total_step:
-			if step % 10 == 0:     # udate the state every 10 seconds
+		while step <= total_step:
+			if step % update_time == 0:     # udate the state every 10 seconds
 				action = agent.select_action(state)  # 0 or 1 (stay or change - simple control logic)
-
+				# print('action: ', action)
 				curr_phase = traci.trafficlight.getPhase(TL_id[0])
 				next_phase = (curr_phase + action) % number_of_phases
 				traci.trafficlight.setPhase(TL_id[0], next_phase)
 				traci.simulationStep()		# executes one step of the simulation (= 1 sec)
 				next_s = TL_state(detectors_ID)
-				reward = TL_reward(next_s)
-				agent.memory(state,reward,next_s,action) # saves state and actions as data to train the neural network
+				reward = TL_reward(detectors_ID, previous_reward)
+				if step == total_step - update_time:
+					done = 0
+				agent.memory(state,reward,next_s,action,done) # saves state and actions as data to train the neural network
 				total_reward += reward
 				state = next_s
+				previous_reward = reward
+				if i > 0:
+					agent.fit_NN()		# fit the NN model, starting from epoch 2
+
 			else:
 				traci.simulationStep()
 
+
 			step += 1
 
+		agent.update_target_network()
 		traci.close()
-		agent.fit_NN()		# fit the NN model
+
+	agent.save('model_weights.h5')
 
 
 
